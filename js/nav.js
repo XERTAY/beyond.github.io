e2e4bb83ad3e8a3465c3588211ecb2dd23cf6703
Vincent <xefal77@gmail.com>
2024-01-17 21:39:15 +0100

    essaie rectification nav 1/3

--- js/nav.js
+++ js/nav.js
@@ -3,12 +3,12 @@ const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener('click', show);
+closeMenu.addEventListener('click', close);
 
 // Ferme le menu
 menu_items.forEach(item => {
-    item.addEventListener('click',function(){
+    item.addEventListener('click', function () {
         close();
     })
 })
@@ -16,15 +16,19 @@ menu_items.forEach(item => {
 function show() {
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
+    mainMenu.classList.add('slideInOut'); // Ajout de la classe pour l'animation à l'ouverture
 
     document.body.style.overflow = "hidden";
-    openMenu.style.display = 'none';
+    openMenu.style.pointerEvents = 'none'; // Désactiver les événements du bouton pendant l'animation
 }
 
 function close() {
     mainMenu.style.top = '-100%';
-    mainMenu.style.display = 'none'
+    document.body.style.overflow = "auto"; // Réactiver le scrolling après le temps d'attente
 
-    document.body.style.overflow = "auto";
-    openMenu.style.display = 'block';
+    setTimeout(() => {
+        mainMenu.style.display = 'none';
+        document.body.style.overflow = "auto";
+        openMenu.style.pointerEvents = 'auto'; // Réactiver les événements du bouton après l'animation
+    }, 1000); // Attendre la fin de l'animation (2 secondes) avant de masquer la barre de navigation
 }
