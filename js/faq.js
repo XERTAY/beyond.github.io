61d4ecd688eb3199e3d6e32026be632cf135431f
Vincent <xefal77@gmail.com>
2024-01-18 22:28:38 +0100

    page logiciel + multiples fixes

---
+++ js/faq.js
@@ -0,0 +1,17 @@
+var content = document.getElementsByClassName("unroll_button");
+var i;
+
+for (i = 0; i < content.length; i++) {
+  content[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("active");
+
+    var pannel = this.nextElementSibling;
+
+    if (pannel.style.display === "block") {
+      pannel.style.display = "none";
+    } else {
+      pannel.style.display = "block";
+    }
+  });
+}
